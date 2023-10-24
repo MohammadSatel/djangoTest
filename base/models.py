dff8bbfb5078970b1b20db59d16bafe8ee712e2b
MohammadSatel <hamuds95@gmail.com>
2023-10-24 20:10:21 +0300

    routes

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    productName = models.CharField(max_length=20,null=True,blank=True)
+    description = models.CharField(max_length=50,null=True,blank=True)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    createdTime=models.DateTimeField(auto_now_add=True)
+    fields =['productName','description','price']
+ 
+    def __str__(self):
+           return self.productName
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
