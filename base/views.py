36e62f337b35c0ec740ece309c085f9c24528abb
MohammadSatel <hamuds95@gmail.com>
2023-10-24 20:18:10 +0300

    test

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from .models import Product
 from .serializer import ProductSerializer
+from rest_framework.decorators import api_view
 
 
 
@@ -15,3 +16,11 @@ def index(req):
 def myProducts(req):
     all_products = ProductSerializer(Product.objects.all(), many=True).data
     return JsonResponse(all_products, safe=False)
+
+@api_view(['GET','POST'])
+def products(request):
+    print(request)
+    tempAr=[]
+    for prod in Product.objects.all():
+        tempAr.append({"description":prod.description,"productName":prod.productName})
+    return HttpResponse(tempAr)
--- project/settings.py
+++ project/settings.py
@@ -38,11 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'rest_framework'
+    'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
